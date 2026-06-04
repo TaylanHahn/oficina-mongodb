6bce611bb47c6898c884f979419a66482b84238f
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2026-06-04 20:34:06 -0300

    Add files via upload

---
+++ testes.mongodb.js
@@ -0,0 +1,9 @@
+use('oficina_mongodb');
+
+// ▶️ Clique em "Run Playground" acima para executar!
+// Selecione um bloco por vez e clique em Run, ou rode tudo de uma vez.
+
+// ===========================================
+// Teste 1: Trazendo todos os livros
+// ===========================================
+db.livros.find();
